ecf71c714b75246aaf6e9da7bd8e895d5ba2b18d
MarcSolde <soldevilla.marc@gmail.com>
2017-03-11 18:28:30 +0100

    UPDATE with a few changes and the connection to a local mongoDB
    the local mongoDB is running on docker, this will have to be refactored in order to use env var or something

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express"),
     app = express(),
     bodyParser  = require("body-parser"),
-    methodOverride = require("method-override");
-mongoose = require('mongoose');
+    methodOverride = require("method-override"),
+    mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -10,12 +10,22 @@ app.use(methodOverride())
 
 var router = express.Router()
 
+var models = require('./model/user')
+var usrCtrl = require('./controllers/user')
+
 router.get('/helloWorld/', function (req, res) {
     res.send("hello")
 })
 
+router.route('/addUser').post(usrCtrl.addUser)
+
 app.use(router)
 
-app.listen(3000, function() {
-    console.log("Node server running on http://localhost:3000");
-});
+mongoose.connect('mongodb://192.168.99.100:27017/pesDB', function (err, res) {
+    if (err) {
+        console.log("Error connecting to the DB")
+    }
+    app.listen(3000, function() {
+        console.log("Node server running on http://localhost:3000");
+    });
+})
