acb558e226dc3f8b235441a19fd0e029ffedd5e8
SergiMuAr <sergimunoz.a@gmail.com>
2017-04-21 20:04:01 +0200

    primera pasada codestyle

--- model/user.model.js
+++ model/user.model.js
@@ -1,42 +1,42 @@
-mongoose = require('mongoose')
+var mongoose = require('mongoose')
 
 var userSchema = new mongoose.Schema({
-    nom: String,
-    nom_user: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password_hash: String,
-    salt: String,
-    path: {
-        type: String,
-        default: ''
-        
-    },
-    localitat: {
-        type: String,
-        default: ''
-    },
-    preferencies: [String],
-    productes: [String],
-    intercanvis: [String],
-    reputacio: {
-        type: String,
-        default: ''
-    },
-    facebook: [
-        {
-            id: String, 
-            name: String,
-        }
-    ],
-    twitter: [
-        {
-            id: String, 
-            name: String
-        }
-    ]
+  nom: String,
+  nom_user: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password_hash: String,
+  salt: String,
+  path: {
+    type: String,
+    default: ''
+
+  },
+  localitat: {
+    type: String,
+    default: ''
+  },
+  preferencies: [String],
+  productes: [String],
+  intercanvis: [String],
+  reputacio: {
+    type: String,
+    default: ''
+  },
+  facebook: [
+    {
+      id: String,
+      name: String
+    }
+  ],
+  twitter: [
+    {
+      id: String,
+      name: String
+    }
+  ]
 
 })
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,19 +1,18 @@
 var express = require('express')
 var router = express.Router()
-var jwt = require('jsonwebtoken')
 var userCtrl = require('../controllers/user.controller')
 var authCtrl = require('../controllers/auth.controller')
 var auth = require('./auth.route')
-//Non-auth
+// Non-auth
 router.use(function (req, res, next) {
-    next()
+  next()
 })
 
 router.post('/addUser', userCtrl.addUser)
 router.post('/login', auth.login)
 router.post('/loginFB', auth.loginFB)
 router.use(authCtrl.checkToken)
-//Auth needed
+// Auth needed
 router.delete('/deleteUser/:nom_user', userCtrl.deleteUser)
 
 module.exports = router
